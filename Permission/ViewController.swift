9ac69a63b8aef45f48b65541aed4ca8b001b2919
poetry by <2638986+btomaj@users.noreply.github.com>
2021-06-02 07:47:38 +1000

    Initial Commit

---
+++ Permission/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Permission
+//
+//  Created by Tomaj Bayat on 21/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+}
